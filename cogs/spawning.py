a8c88cfc3a1f34605c71c1e5e778c3db7d81b030
Oliver Ni <oliver.ni@gmail.com>
2020-06-09 19:13:08 -0700

    Fix giveaway prompt

--- cogs/spawning.py
+++ cogs/spawning.py
@@ -172,11 +172,6 @@ class Spawning(commands.Cog):
         )
         embed.set_image(url="attachment://pokemon.png")
 
-        if channel.guild.id != 716390832034414685:
-            embed.set_footer(
-                text="We're giving out a Sky Shaymin! Join the official server using `p!invite` to enter."
-            )
-
         await channel.send(file=image, embed=embed)
 
     @checks.has_started()
