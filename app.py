cfeb5b6b33e705c46dbc4b1434dd360d7e5b595c
Oliver Ni <oliver.ni@gmail.com>
2020-06-12 02:48:27 -0700

    Add processing

--- app.py
+++ app.py
@@ -1,23 +1,46 @@
-from flask import Flask, request, abort
+import asyncio
 import hmac
 import os
+
+from flask import Flask, abort, request
+
+from cogs import mongo
 
 secret = os.getenv("PATREON_SECRET").encode("ascii")
 
 app = Flask(__name__)
 
+
 @app.route("/patreon", methods=["POST"])
 def webhook():
-    if request.method == "POST":
-        digest = hmac.new(secret, request.data, digestmod="md5")
-        if not hmac.compare_digest(digest.hexdigest(), request.headers.get("X-Patreon-Signature")):
-            abort(403)
+    digest = hmac.new(secret, request.data, digestmod="md5").hexdigest()
+    given = request.headers.get("X-Patreon-Signature")
 
-        print(request.json)
+    if not hmac.compare_digest(digest, given):
+        abort(403)
 
-        return "", 200
-    else:
-        abort(400)
+    user = [x for x in request.json["included"] if x["type"] == "user"][0]
+    rewards = [x for x in request.json["included"] if x["type"] == "reward"]
+
+    inc_redeems = 0
+
+    for reward in rewards:
+        if reward["id"] == "2608556":
+            inc_redeems += 1
+        if reward["id"] == "2610442":
+            inc_redeems += 4
+        if reward["id"] == "2611242":
+            inc_redeems += 8
+
+    update = mongo.db.member.update_one(
+        {"_id": int(user["attributes"]["discord_id"])},
+        {"$inc": {"redeems": inc_redeems}},
+    )
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(update)
+
+    return "", 200
 
 
 if __name__ == "__main__":
