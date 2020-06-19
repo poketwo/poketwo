bc8395e5a5fe7f114de5b3c9074eec823921c0e3
Oliver Ni <oliver.ni@gmail.com>
2020-06-18 19:04:05 -0700

    Comment patreon

--- api/app.py
+++ api/app.py
@@ -5,7 +5,7 @@ import os
 from flask import Flask, abort, request
 from pymongo import MongoClient
 
-secret = os.getenv("PATREON_SECRET").encode("ascii")
+# secret = os.getenv("PATREON_SECRET").encode("ascii")
 db = MongoClient(os.getenv("DATABASE_URI"))[os.getenv("DATABASE_NAME")]
 
 app = Flask(__name__)
@@ -16,45 +16,45 @@ def dbl():
     print(request.json)
 
 
-@app.route("/patreon", methods=["POST"])
-def patreon():
-    if "delete" in request.headers.get("X-Patreon-Event"):
-        return "", 200
+# @app.route("/patreon", methods=["POST"])
+# def patreon():
+#     if "delete" in request.headers.get("X-Patreon-Event"):
+#         return "", 200
 
-    digest = hmac.new(secret, request.data, digestmod="md5").hexdigest()
-    given = request.headers.get("X-Patreon-Signature")
+#     digest = hmac.new(secret, request.data, digestmod="md5").hexdigest()
+#     given = request.headers.get("X-Patreon-Signature")
 
-    if not hmac.compare_digest(digest, given):
-        abort(403)
+#     if not hmac.compare_digest(digest, given):
+#         abort(403)
 
-    data = request.json["data"]
-    user = [x for x in request.json["included"] if x["type"] == "user"][0]
-    rewards = [x for x in request.json["included"] if x["type"] == "reward"]
+#     data = request.json["data"]
+#     user = [x for x in request.json["included"] if x["type"] == "user"][0]
+#     rewards = [x for x in request.json["included"] if x["type"] == "reward"]
 
-    inc_redeems = 0
+#     inc_redeems = 0
 
-    for reward in rewards:
-        if reward["id"] == "2608556":
-            inc_redeems += 1
-        if reward["id"] == "2610442":
-            inc_redeems += 4
-        if reward["id"] == "2611242":
-            inc_redeems += 8
+#     for reward in rewards:
+#         if reward["id"] == "2608556":
+#             inc_redeems += 1
+#         if reward["id"] == "2610442":
+#             inc_redeems += 4
+#         if reward["id"] == "2611242":
+#             inc_redeems += 8
 
-    res = db.member.update_one(
-        {"_id": int(user["attributes"]["discord_id"])},
-        {"$inc": {"redeems": inc_redeems}},
-    )
+#     res = db.member.update_one(
+#         {"_id": int(user["attributes"]["discord_id"])},
+#         {"$inc": {"redeems": inc_redeems}},
+#     )
 
-    print(
-        "Giving "
-        + user["attributes"]["discord_id"]
-        + " "
-        + str(inc_redeems)
-        + " redeems."
-    )
+#     print(
+#         "Giving "
+#         + user["attributes"]["discord_id"]
+#         + " "
+#         + str(inc_redeems)
+#         + " redeems."
+#     )
 
-    return "", 200
+#     return "", 200
 
 
 if __name__ == "__main__":
