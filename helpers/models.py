94a6b24e6de1b6629daa928fab1a27451ddd0131
Oliver Ni <oliver.ni@gmail.com>
2020-08-22 22:43:30 -0700

    Update shiny emotes

--- helpers/models.py
+++ helpers/models.py
@@ -369,11 +369,11 @@ class Species:
 
     @cached_property
     def image_url(self):
-        return f"https://assets.poketwo.net/images/{self.id}.png?v=800"
+        return f"https://assets.poketwo.net/images/{self.id}.png?v=900"
 
     @cached_property
     def shiny_image_url(self):
-        return f"https://assets.poketwo.net/shiny/{self.id}.png?v=800"
+        return f"https://assets.poketwo.net/shiny/{self.id}.png?v=900"
 
     @cached_property
     def correct_guesses(self):
