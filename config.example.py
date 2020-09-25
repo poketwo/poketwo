73b49b003e08b2fefb1ca121beae9007180af99c
Oliver Ni <oliver.ni@gmail.com>
2020-09-25 11:47:27 -0700

    Add example config

---
+++ config.example.py
@@ -0,0 +1,17 @@
+# Database Configuration
+DATABASE_URI = None
+DATABASE_NAME = None
+
+# Bot Configuration
+BOT_TOKEN = None
+ENV = "dev"
+SECRET_KEY = None
+
+# DBL
+DBL_TOKEN = None
+DBL_SECRET = None
+
+# Webhooks
+STRIPE_KEY = None
+STRIPE_WEBHOOK_SECRET = None
+GITHUB_WEBHOOK_SECRET = None
