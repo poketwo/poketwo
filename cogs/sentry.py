7147162d555829eb120392eef8eeae1f37f6072e
Oliver Ni <oliver.ni@gmail.com>
2020-11-11 04:08:34 -0800

    Filter errors

--- cogs/sentry.py
+++ cogs/sentry.py
@@ -1,5 +1,8 @@
 import sentry_sdk
-from discord.ext import commands
+from discord.ext import commands, flags
+
+from helpers import converters
+from .bot import Blacklisted
 
 
 class Sentry(commands.Cog):
@@ -8,10 +11,24 @@ class Sentry(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         sentry_sdk.init(bot.config.SENTRY_URL, traces_sample_rate=1.0)
-    
+
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        sentry_sdk.capture_exception(error)
+        if not isinstance(
+            error,
+            (
+                Blacklisted,
+                commands.CommandOnCooldown,
+                commands.MaxConcurrencyReached,
+                commands.DisabledCommand,
+                commands.ConversionError,
+                commands.CheckFailure,
+                commands.UserInputError,
+                commands.CommandNotFound,
+                flags.ArgumentParsingError,
+            ),
+        ):
+            sentry_sdk.capture_exception(error)
 
     @commands.Cog.listener()
     async def on_error(self, error):
