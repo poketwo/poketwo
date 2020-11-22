6ee1848febec6d24c2cec384485d7b196e26b6bb
Oliver Ni <oliver.ni@gmail.com>
2020-11-22 03:01:22 -0800

    Improve market logs

--- cogs/market.py
+++ cogs/market.py
@@ -330,6 +330,7 @@ class Market(commands.Cog):
                     "event": "market",
                     "user": ctx.author.id,
                     "item": listing["pokemon"]["_id"],
+                    "seller_id": listing["user_id"],
                     "price": listing["price"],
                 }
             )
