404d958ba5dd226edf3dff3eebae869176eb90df
william1657 <76636792+william1657@users.noreply.github.com>
2020-12-29 11:08:32 -0600

    Trying to implement Issue #75

--- cogs/spawning.py
+++ cogs/spawning.py
@@ -236,6 +236,11 @@ class Spawning(commands.Cog):
             self.spawn_threshold *= 1.1
 
     async def spawn_pokemon(self, channel, species=None, incense=None):
+        if await self.bot.redis.hexists("wild", channel.id):
+            prev_species_id = await self.bot.redis.hget("wild", channel.id)
+            prev_species = self.bot.data.species_by_number(int(prev_species_id))
+            await channel.send(f"Wild {prev_species} fled.")
+            
         if species is None:
             species = self.bot.data.random_spawn()
 
