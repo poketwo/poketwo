3d2e4c6a97ed242f8f48a52d3341b7dffefd4f77
Kyle He <panyanhe99@gmail.com>
2021-03-31 10:28:33 -0700

    add gen8 (not clickbait)

--- cogs/spawning.py
+++ cogs/spawning.py
@@ -69,7 +69,7 @@ class Spawning(commands.Cog):
                     self.spawn_pokemon(channel, incense=result["spawns_remaining"])
                 )
                 await self.bot.mongo.update_channel(channel, {"$inc": {"spawns_remaining": -1}})
-
+    
     @spawn_incense.before_loop
     async def before_spawn_incense(self):
         await self.bot.wait_until_ready()
@@ -396,6 +396,8 @@ class Spawning(commands.Cog):
 
         else:
             inc_bal = 0
+
+            # catching streaks
 
             if memberp.pokedex[str(species.dex_number)] + 1 == 10:
                 message += f" This is your 10th {self.bot.data.species_by_number(species.dex_number)}! You received 350 Pokécoins."
