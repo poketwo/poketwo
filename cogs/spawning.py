232069fffc132d4bb5f5dd177139a39810fafd84
witherr <>
2021-05-04 16:52:48 +0000

    Made it so you can't p!sh the Pokemon you're already hunting.
    (sorry if any of the code is faulty)

--- cogs/spawning.py
+++ cogs/spawning.py
@@ -448,20 +448,26 @@ class Spawning(commands.Cog):
 
         member = await self.bot.mongo.fetch_member_info(ctx.author)
 
+        # variable for current shiny hunt
+        current_hunting = self.bot.data.species_by_number(member.shiny_hunt).species_by_name
+        
+        # variable for current shiny hunt streak
+        current_streak = member.shiny_streak
+        
         if species is None:
             embed = self.bot.Embed(color=0xFE9AC9)
             embed.title = f"Shiny Hunt ✨"
             embed.description = "You can select a specific pokémon to shiny hunt. Each time you catch that pokémon, your chain will increase. The longer your chain, the higher your chance of catching a shiny one!"
-
+            
             embed.add_field(
                 name=f"Currently Hunting",
                 value=self.bot.data.species_by_number(member.shiny_hunt).name
                 if member.shiny_hunt
                 else "Type `p!shinyhunt <pokémon>` to begin!",
             )
-
+          
             if member.shiny_hunt:
-                embed.add_field(name=f"Chain", value=str(member.shiny_streak))
+                embed.add_field(name=f"Chain", value=str(current_streak))
 
             return await ctx.send(embed=embed)
 
@@ -472,12 +478,15 @@ class Spawning(commands.Cog):
 
         if not species.catchable:
             return await ctx.send("This pokémon can't be caught in the wild!")
-
-        if member.shiny_streak > 0:
+            
+        # if statement incase species is the same as current shinyhunt
+        if species == current_hunting:
+            return await ctx.send(f"You are already hunting this pokémon with a streak of {current_streak}.")
+            
+        if current_streak > 0 and not species == current_hunting:
             await ctx.send(
                 f"Are you sure you want to shiny hunt a different pokémon? Your streak will be reset. [y/N]"
-            )
-
+                )
             def check(m):
                 return m.channel.id == ctx.channel.id and m.author.id == ctx.author.id
 
