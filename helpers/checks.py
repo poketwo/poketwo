b83323b4ba87b854893ffc67b9a1edaf11307e98
Oliver Ni <oliver.ni@gmail.com>
2021-10-10 22:21:10 -0700

    Update helpers/checks.py

--- helpers/checks.py
+++ helpers/checks.py
@@ -37,6 +37,5 @@ def is_not_in_trade():
     def predicate(ctx):
         if ctx.bot.get_cog("Trading").is_in_trade(ctx.author):
             raise commands.CheckFailure("You can't do that in a trade!")
-        else:
-            return True
+        return True
     return commands.check(predicate)
