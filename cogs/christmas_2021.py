4b8b7fb464a314e7f3ae8191da01e987bd7e5997
witherr <81734495+WitherredAway@users.noreply.github.com>
2021-12-28 19:57:24 +0000

    Ability to open multiple boxes at once

--- cogs/christmas_2021.py
+++ cogs/christmas_2021.py
@@ -64,7 +64,7 @@ class Christmas(commands.Cog):
             )
 
     @checks.has_started()
-    @commands.group(aliases=("event",), invoke_without_command=True, case_insensitive=True)
+    @commands.group(aliases=("event", "ev"), invoke_without_command=True, case_insensitive=True)
     async def christmas(self, ctx):
         """View Christmas event information."""
 
@@ -102,87 +102,105 @@ class Christmas(commands.Cog):
 
     @checks.has_started()
     @commands.max_concurrency(1, commands.BucketType.user)
-    @christmas.command()
-    async def open(self, ctx, box_type):
+    @christmas.command(aliases=("o"))
+    async def open(self, ctx, box_type: str = "", amt: int = 1):
         """Open a box"""
+
+        if amt <= 0:
+            return await ctx.send("Nice try...")
+
+        if amt > 15:
+            return await ctx.send("You can only open 15 event boxes at once!")
+        
+        box_type = box_type.lower()
+        if box_type not in {"nice", "naughty"}:
+            return await ctx.send("Please type `nice` or `naughty`!")
 
         member = await self.bot.mongo.fetch_member_info(ctx.author)
 
-        box_type = box_type.lower()
-        if box_type not in {"nice", "naughty"}:
-            return await ctx.send("Need a valid box type!")
-
-        if box_type == "nice" and member.christmas_boxes_nice <= 0:
-            return await ctx.send("You don't have enough boxes to do that!")
-        if box_type == "naughty" and member.christmas_boxes_naughty <= 0:
-            return await ctx.send("You don't have enough boxes to do that!")
+        if box_type == "nice" and member.christmas_boxes_nice < amt:
+            return await ctx.send("You don't have enough nice boxes to do that!")
+        if box_type == "naughty" and member.christmas_boxes_naughty < amt:
+            return await ctx.send("You don't have enough naughty boxes to do that!")
 
         await self.bot.mongo.update_member(
             ctx.author,
-            {"$inc": {f"christmas_boxes_{box_type}": -1, f"christmas_boxes_{box_type}_opened": 1}},
+            {"$inc": {f"christmas_boxes_{box_type}": -amt, f"christmas_boxes_{box_type}_opened": amt}},
         )
 
         # Go
 
         if box_type == "nice":
-            reward = random.choices(*NICE, k=1)[0]
+            rewards = random.choices(*NICE, k=amt)
         else:
-            reward = random.choices(*NAUGHTY, k=1)[0]
-
-        if reward == "shards":
-            shards = max(round(random.normalvariate(25, 10)), 2)
-            await self.bot.mongo.update_member(ctx.author, {"$inc": {"premium_balance": shards}})
-            text = f"{shards} Shards"
-
-        elif reward == "pokecoins":
-            pokecoins = max(round(random.normalvariate(1000, 500)), 800)
-            await self.bot.mongo.update_member(ctx.author, {"$inc": {"balance": pokecoins}})
-            text = f"{pokecoins} Pokécoins"
-
-        elif reward == "redeem":
-            await self.bot.mongo.update_member(ctx.author, {"$inc": {"redeems": 1}})
-            text = "1 redeem"
-
-        elif reward in ("event", "pokemon", "rare", "shiny"):
-            pool = [x for x in self.pools[reward] if x.catchable or reward == "event"]
-            species = random.choices(pool, weights=[x.abundance for x in pool], k=1)[0]
-            level = min(max(int(random.normalvariate(30, 10)), 1), 100)
-            shiny = reward == "shiny" or member.determine_shiny(species)
-            ivs = [mongo.random_iv() for i in range(6)]
-
-            pokemon = {
-                "owner_id": ctx.author.id,
-                "owned_by": "user",
-                "species_id": species.id,
-                "level": level,
-                "xp": 0,
-                "nature": mongo.random_nature(),
-                "iv_hp": ivs[0],
-                "iv_atk": ivs[1],
-                "iv_defn": ivs[2],
-                "iv_satk": ivs[3],
-                "iv_sdef": ivs[4],
-                "iv_spd": ivs[5],
-                "iv_total": sum(ivs),
-                "shiny": shiny,
-                "idx": await self.bot.mongo.fetch_next_idx(ctx.author),
-            }
-
-            text = (
-                f"{self.bot.mongo.Pokemon.build_from_mongo(pokemon):lni} ({sum(ivs) / 186:.2%} IV)"
-            )
-
-            await self.bot.mongo.db.pokemon.insert_one(pokemon)
-
-        else:
-            text = "Nothing"
+            rewards = random.choices(*NAUGHTY, k=amt)
 
         embed = self.bot.Embed(
-            title=f"🎁 {box_type.title()} Box Reward",
-            description=text,
+            title=f"Opening {amt} 🎁 {box_type.title()} Box{'es' if amt > 1 else ''}...",
+            color=random.choice([0x9ECFFC, 0xDE2E43, 0x79B15A])
         )
-        embed.set_author(icon_url=ctx.author.display_avatar.url, name=str(ctx.author))
+        
+        update = {
+            "$inc": {"premium_balance": 0, "balance": 0, "redeems": 0},
+        }
+        
+        text = []
+        added_pokemon = []
+        for reward in rewards:
+            if reward == "shards":
+                shards = max(round(random.normalvariate(25, 10)), 2)
+                update["$inc"]["premium_balance"] += shards
+                text.append(f"{shards} Shards")
 
+            elif reward == "pokecoins":
+                pokecoins = max(round(random.normalvariate(1000, 500)), 800)
+                update["$inc"]["balance"] += pokecoins
+                text.append(f"{pokecoins} Pokécoins")
+
+            elif reward == "redeem":
+                update["$inc"]["redeems"] += 1
+                text.append("1 redeem")
+
+            elif reward in ("event", "pokemon", "rare", "shiny"):
+                pool = [x for x in self.pools[reward] if x.catchable or reward == "event"]
+                species = random.choices(pool, weights=[x.abundance for x in pool], k=1)[0]
+                level = min(max(int(random.normalvariate(30, 10)), 1), 100)
+                shiny = reward == "shiny" or member.determine_shiny(species)
+                ivs = [mongo.random_iv() for i in range(6)]
+
+                pokemon = {
+                    "owner_id": ctx.author.id,
+                    "owned_by": "user",
+                    "species_id": species.id,
+                    "level": level,
+                    "xp": 0,
+                    "nature": mongo.random_nature(),
+                    "iv_hp": ivs[0],
+                    "iv_atk": ivs[1],
+                    "iv_defn": ivs[2],
+                    "iv_satk": ivs[3],
+                    "iv_sdef": ivs[4],
+                    "iv_spd": ivs[5],
+                    "iv_total": sum(ivs),
+                    "shiny": shiny,
+                    "idx": await self.bot.mongo.fetch_next_idx(ctx.author),
+                }
+
+                text.append(
+                    f"{self.bot.mongo.Pokemon.build_from_mongo(pokemon):lni} ({sum(ivs) / 186:.2%} IV)"
+                )
+                
+                added_pokemon.append(pokemon)
+
+            else:
+                text.append("Nothing")
+
+        embed.add_field(name="Rewards Received", value="\n".join(text))
+        embed.set_author(icon_url=ctx.author.display_avatar.url, name=str(ctx.author))
+        
+        await self.bot.mongo.update_member(ctx.author, update)
+        if len(added_pokemon) > 0:
+            await self.bot.mongo.db.pokemon.insert_many(added_pokemon)
         await ctx.send(embed=embed)
 
 
