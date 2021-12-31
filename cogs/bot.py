4c8ca32f9149d22c0899dcd87fc6d352dc11b4e5
Yihong(Michael) Song <46873721+michaelsong4399@users.noreply.github.com>
2021-12-30 21:59:32 -0500

    Update Suspension Appeals Message

--- cogs/bot.py
+++ cogs/bot.py
@@ -103,10 +103,9 @@ class Bot(commands.Cog):
             embed = discord.Embed(
                 color=discord.Color.red(),
                 title="Account Suspended",
-                description="Your account was found to be in violation of Pokétwo rules and has been permanently blacklisted from using the bot.",
+                description="Your account was found to be in violation of Pokétwo rules and has been permanently blacklisted from using the bot. If you would like to appeal, [click here](https://forms.poketwo.net/a/suspension-appeal).",
             )
             embed.add_field(name="Reason", value=error.reason or "No reason provided", inline=False)
-            embed.add_field(name="", value="[Appeal Suspension](https://forms.poketwo.net/a/suspension-appeal)", inline=False)
             await ctx.send(embed=embed)
         elif isinstance(
             error,
