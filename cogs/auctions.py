0d2dfefcc54ef732b54ac0720d607eaa6e1bb75e
Oliver Ni <oliver.ni@gmail.com>
2022-05-28 10:26:31 -0700

    Add bid price to auction outbid message

--- cogs/auctions.py
+++ cogs/auctions.py
@@ -416,7 +416,7 @@ class Auctions(commands.Cog):
             self.bot.loop.create_task(
                 self.bot.send_dm(
                     auction.bidder_id,
-                    f"You have been outbid on the **{auction.pokemon.iv_percentage:.2%} {auction.pokemon.species}** (Auction #{auction.id}).",
+                    f"You have been outbid on the **{auction.pokemon.iv_percentage:.2%} {auction.pokemon.species}** (Auction #{auction.id}). New bid: {bid} pokécoins.",
                 )
             )
         await ctx.send(
