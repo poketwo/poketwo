14644dbe67bf6434a9371fd1d0c059df809f3370
Oliver Ni <oliver.ni@gmail.com>
2022-09-07 15:05:10 -0700

    Add select menu helper

--- helpers/context.py
+++ helpers/context.py
@@ -2,6 +2,38 @@ import discord
 from discord.ext import commands
 
 
+class Select(discord.ui.Select):
+    async def callback(self, interaction):
+        await interaction.response.defer()
+        if self.view.message:
+            await self.view.message.edit(view=None)
+        self.view.result = self.values
+        await self.view.stop()
+
+
+class SelectView(discord.ui.View):
+    def __init__(self, ctx, *, options: list[discord.SelectOption], timeout) -> None:
+        super().__init__(timeout=timeout)
+        self.result = None
+        self.ctx = ctx
+        self.message = None
+        self.add_item(Select(options=options))
+
+    async def interaction_check(self, interaction):
+        if interaction.user.id not in {
+            self.ctx.bot.owner_id,
+            self.ctx.author.id,
+            *self.ctx.bot.owner_ids,
+        }:
+            await interaction.response.send_message("You can't use this!", ephemeral=True)
+            return False
+        return True
+
+    async def on_timeout(self):
+        if self.message:
+            await self.message.delete()
+
+
 class ConfirmationView(discord.ui.View):
     def __init__(self, ctx, *, timeout) -> None:
         super().__init__(timeout=timeout)
@@ -43,6 +75,19 @@ class ConfirmationView(discord.ui.View):
 class PoketwoContext(commands.Context):
     async def confirm(self, message=None, *, embed=None, timeout=40, cls=ConfirmationView):
         view = cls(self, timeout=timeout)
+        view.message = await self.send(
+            message,
+            embed=embed,
+            view=view,
+            allowed_mentions=discord.AllowedMentions.none(),
+        )
+        await view.wait()
+        return view.result
+
+    async def select(
+        self, message=None, *, embed=None, timeout=40, options: list[discord.SelectOption], cls=SelectView
+    ):
+        view = cls(self, options=options, timeout=timeout)
         view.message = await self.send(
             message,
             embed=embed,
