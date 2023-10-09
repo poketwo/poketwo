6776eba24fe32c8090e6ab50671b22338e48d030
witherr <sdssouvic99@gmail.com>
2023-10-10 00:02:39 +0530

    Unify views

--- helpers/context.py
+++ helpers/context.py
@@ -4,8 +4,10 @@ import typing
 from typing import Optional
 
 import discord
-import structlog
 from discord.ext import commands
+
+if typing.TYPE_CHECKING:
+    from bot import ClusterBot
 
 
 class Select(discord.ui.Select):
@@ -44,107 +46,77 @@ class SelectView(discord.ui.View):
             await self.message.delete()
 
 
+class ConfirmationButton(discord.ui.Button):
+    def __init__(self, *, label: str, result: bool, style: discord.ButtonStyle):
+        self.result = result
+        super().__init__(label=label, style=style)
+
+    async def callback(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+        if self.view.message:
+            await self.view.message.edit(view=None)
+        self.view.result = self.result
+        self.view.stop()
+        if self.view.delete_after:
+            await self.view.message.delete()
+
+
 class ConfirmationView(discord.ui.View):
-    def __init__(self, ctx, *, timeout, delete_after) -> None:
-        super().__init__(timeout=timeout)
-        self.result = None
-        self.ctx = ctx
-        self.message = None
-        self.delete_after = delete_after
-
-    async def interaction_check(self, interaction):
-        if interaction.user.id not in {
-            self.ctx.bot.owner_id,
-            self.ctx.author.id,
-            *self.ctx.bot.owner_ids,
-        }:
-            await interaction.response.send_message("You can't use this!", ephemeral=True)
-            return False
-        return True
-
-    @discord.ui.button(label="Confirm", style=discord.ButtonStyle.green)
-    async def confirm(self, interaction, button):
-        await interaction.response.defer()
-        if self.message:
-            await self.message.edit(view=None)
-        self.result = True
-        self.stop()
-        if self.delete_after:
-            await self.message.delete()
-
-    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.red)
-    async def cancel(self, interaction, button):
-        await interaction.response.defer()
-        if self.message:
-            await self.message.edit(view=None)
-        self.result = False
-        self.stop()
-        if self.delete_after:
-            await self.message.delete()
-
-    async def on_timeout(self):
-        if self.message:
-            await self.message.delete()
-
-
-class ConfirmationYesNoView(discord.ui.View):
-    def __init__(self, ctx, *, timeout, delete_after) -> None:
-        super().__init__(timeout=timeout)
-        self.result = None
-        self.ctx = ctx
-        self.message = None
-        self.delete_after = delete_after
-
-    async def interaction_check(self, interaction):
-        if interaction.user.id not in {
-            self.ctx.bot.owner_id,
-            self.ctx.author.id,
-            *self.ctx.bot.owner_ids,
-        }:
-            await interaction.response.send_message("You can't use this!", ephemeral=True)
-            return False
-        return True
-
-    @discord.ui.button(label="Yes")
-    async def confirm(self, interaction, button):
-        await interaction.response.defer()
-        if self.message:
-            await self.message.edit(view=None)
-        self.result = True
-        self.stop()
-        if self.delete_after:
-            await self.message.delete()
-
-    @discord.ui.button(label="No")
-    async def cancel(self, interaction, button):
-        await interaction.response.defer()
-        if self.message:
-            await self.message.edit(view=None)
-        self.result = False
-        self.stop()
-        if self.delete_after:
-            await self.message.delete()
-
-    async def on_timeout(self):
-        if self.message:
-            await self.message.delete()
-
-
-class RequestView(discord.ui.View):
     def __init__(
         self,
         ctx: PoketwoContext,
-        requestee: typing.Union[discord.User, discord.Member],
         *,
         timeout: int,
-        delete_after: bool
+        delete_after: bool,
+        delete_after_timeout: bool
     ) -> None:
         super().__init__(timeout=timeout)
         self.result = None
         self.ctx = ctx
-        self.requestee = requestee
         self.message = None
         self.delete_after = delete_after
+        self.delete_after_timeout = delete_after_timeout
+
+        self.add_items()
+
+    async def interaction_check(self, interaction):
+        if interaction.user.id not in {
+            self.ctx.bot.owner_id,
+            self.ctx.author.id,
+            *self.ctx.bot.owner_ids,
+        }:
+            await interaction.response.send_message("You can't use this!", ephemeral=True)
+            return False
+        return True
+
+    def add_items(self):
+        """The method that adds the buttons. This needs to be implemented in subclasses in order to change labels."""
+        self.add_item(ConfirmationButton(label="Confirm", result=True, style=discord.ButtonStyle.green))
+        self.add_item(ConfirmationButton(label="Cancel", result=False, style=discord.ButtonStyle.red))
+
+    async def on_timeout(self):
+        if self.message:
+            if self.delete_after_timeout:
+                await self.message.delete()
+            else:
+                await self.message.edit(view=None)
+
+
+class ConfirmationYesNoView(ConfirmationView):
+    def add_items(self):
+        self.add_item(ConfirmationButton(label="Yes", result=True, style=discord.ButtonStyle.green))
+        self.add_item(ConfirmationButton(label="No", result=False, style=discord.ButtonStyle.red))
+
+
+class RequestView(ConfirmationView):
+    def __init__(
+        self,
+        *args,
+        requestee: typing.Union[discord.User, discord.Member],
+        **kwargs
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self.requestee = requestee
 
     async def interaction_check(self, interaction):
         if interaction.user.id not in {
@@ -156,29 +128,9 @@ class RequestView(discord.ui.View):
             return False
         return True
 
-    @discord.ui.button(label="Accept", style=discord.ButtonStyle.green)
-    async def accept(self, interaction, button):
-        await interaction.response.defer()
-        if self.message:
-            await self.message.edit(view=None)
-        self.result = True
-        self.stop()
-        if self.delete_after:
-            await self.message.delete()
-
-    @discord.ui.button(label="Reject", style=discord.ButtonStyle.red)
-    async def reject(self, interaction, button):
-        await interaction.response.defer()
-        if self.message:
-            await self.message.edit(view=None)
-        self.result = False
-        self.stop()
-        if self.delete_after:
-            await self.message.delete()
-
-    async def on_timeout(self):
-        if self.message:
-            await self.message.edit(view=None)
+    def add_items(self):
+        self.add_item(ConfirmationButton(label="Accept", result=True, style=discord.ButtonStyle.green))
+        self.add_item(ConfirmationButton(label="Reject", result=False, style=discord.ButtonStyle.red))
 
 
 class PoketwoContext(commands.Context):
@@ -199,9 +151,17 @@ class PoketwoContext(commands.Context):
         )
 
     async def confirm(
-        self, message=None, *, file=None, embed=None, timeout=40, delete_after=False, cls=ConfirmationView
+        self,
+        message: Optional[discord.Message] = None,
+        *,
+        file: Optional[discord.File] = None,
+        embed: Optional[ClusterBot.Embed] = None,
+        timeout: Optional[int] = 40,
+        delete_after: Optional[bool] = False,
+        delete_after_timeout: Optional[bool] = True,
+        cls: Optional[ConfirmationView] = ConfirmationView
     ):
-        view = cls(self, timeout=timeout, delete_after=delete_after)
+        view = cls(self, timeout=timeout, delete_after=delete_after, delete_after_timeout=delete_after_timeout)
         view.message = await self.send(
             message,
             file=file,
@@ -220,9 +180,10 @@ class PoketwoContext(commands.Context):
         file: Optional[discord.File] = None,
         embed: Optional[discord.Embed] = None,
         timeout: Optional[int] = 40,
-        delete_after: Optional[bool] = False
+        delete_after: Optional[bool] = False,
+        delete_after_timeout: Optional[bool] = False,
     ):
-        view = RequestView(self, requestee, timeout=timeout, delete_after=delete_after)
+        view = RequestView(self, requestee=requestee, timeout=timeout, delete_after=delete_after, delete_after_timeout=delete_after_timeout)
         view.message = await self.send(
             message,
             file=file,
