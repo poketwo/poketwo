8da1583b80de65e49fded3f0ebdc7158deffd4b3
witherr <sdssouvic99@gmail.com>
2024-09-24 00:04:23 +0530

    Add loading the event cog

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -16,5 +16,6 @@ default = (
     "incenses",
     "sprites",
     "trading",
-    "noevent",  # Comment this line before adding new event cogs, and add it back when ending events.
+    # "noevent",  # Comment this line before adding new event cogs, and add it back when ending events.
+    "event",  # Cog for simple events, remove when bigger event cogs are added.
 )
