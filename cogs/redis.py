9b50dd4a5fbeb2629d65f1b1a4c681ed8ca1fec6
witherr <sdssouvic99@gmail.com>
2024-10-13 14:09:40 +0530

    Add loop to attempt to reconnect redis if not connected

--- cogs/redis.py
+++ cogs/redis.py
@@ -1,5 +1,5 @@
 import aioredis
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 
 class Redis(commands.Cog):
@@ -9,9 +9,21 @@ class Redis(commands.Cog):
         self.bot = bot
         self.pool = None
         self._connect_task = self.bot.loop.create_task(self.connect())
+        self.attempt_reconnect.start()
 
     async def connect(self):
         self.pool = await aioredis.create_redis_pool(**self.bot.config.REDIS_CONF)
+
+    @tasks.loop(seconds=0.1)
+    async def attempt_reconnect(self):
+        if self.pool:
+            return
+
+        await self.connect()
+
+    @attempt_reconnect.before_loop
+    async def before_attempt_reconnect(self):
+        await self.bot.wait_until_ready()
 
     async def close(self):
         self.pool.close()
@@ -21,6 +33,7 @@ class Redis(commands.Cog):
         await self._connect_task
 
     def cog_unload(self):
+        self.attempt_reconnect.cancel()
         self.bot.loop.create_task(self.close())
 
 
