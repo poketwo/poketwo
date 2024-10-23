9b55272113109758bd25925c93c47db432b700cd
witherr <sdssouvic99@gmail.com>
2024-10-24 00:10:44 +0530

    End autumn event

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -16,6 +16,6 @@ default = (
     "incenses",
     "sprites",
     "trading",
-    # "noevent",  # Comment this line before adding new event cogs, and add it back when ending events.
-    "event",  # Cog for simple events, remove when bigger event cogs are added.
+    # "event",  # Cog for simple events, remove when bigger event cogs are added.
+    "noevent",  # Comment this line before adding new event cogs, and add it back when ending events.
 )
